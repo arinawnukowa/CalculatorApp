6c7f623393bc89396ef8e01af2e58c2fa000407c
arinawnukowa <arinawnukowa@gmail.com>
2026-05-14 01:05:50 +0300

    Initial commit with CalculatorApp

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CalculatorApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+            int result = calc.Add(5, 3);
+            Console.WriteLine($"5 + 3 = {result}");
+            Console.ReadKey();
+        }
+    }
+}
