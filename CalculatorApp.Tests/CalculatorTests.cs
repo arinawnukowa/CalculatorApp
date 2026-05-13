0b0a0b673e30ee06f94870e6fa98b02acfde7b5a
arinawnukowa <arinawnukowa@gmail.com>
2026-05-14 02:19:05 +0300

    Add CalculatorApp.Tests project

---
+++ CalculatorApp.Tests/CalculatorTests.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using CalculatorApp;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnSum_WhenTwoNumbersAreAdded()
+        {
+            // Arrange
+            Calculator calculator = new Calculator();
+
+            // Act
+            int result = calculator.Add(5, 3);
+
+            // Assert
+            Assert.Equal(8, result);
+        }
+    }
+}
